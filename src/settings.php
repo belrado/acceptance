eecb4df1c1f7ecca251292b37757cf2ea8baa9c0
bernardo <u5ink@naver.com>
2019-08-01 12:04:11 +0900

    settings.php db 접속 정부 추가

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,13 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Databases settings
+        'db'        => [
+            'host'      => 'localhost',
+            'user'      => 'psuapi_db',
+            'pass'      => 'api_db_sql!',
+            'dbname'    => 'psuapi_db'
+        ],
     ],
 ];
