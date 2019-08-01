b3e2f6bc68889dd51c468ea857dc714145b96f5a
bernardo <u5ink@naver.com>
2019-08-01 12:03:40 +0900

    dependencies.php db 커넥션 추가 ped mysqli

--- src/dependencies.php
+++ src/dependencies.php
@@ -19,4 +19,24 @@ return function (App $app) {
         $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
         return $logger;
     };
+
+    // db pdo
+    $container['db_pdo'] = function ($c) {
+        $db = $c->get('settings')['db'];
+        $con = new PDO('mysql:host=' . $db['host'] . ';port='.$db['port'].';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $con;
+    };
+
+    // db mysqli
+    $container['db_mysqli'] = function ($c) {
+        $db = $c->get('settings')['db'];
+        $con = new mysqli($db['host'], $db['user'], $db['pass'], $db['dbname'], $db['port']);
+        if(!$con->connect_error){
+            return $con;
+        }else{
+            return false;
+        }
+    };
 };
